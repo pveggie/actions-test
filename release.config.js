af25dcc3314f335d717df71ff9b1aace454a534b
Gemma Gotch <g.gotch@chi-deutschland.com>
2024-03-15 15:25:16 +0100

    add config for pushing version update

--- release.config.js
+++ release.config.js
@@ -20,6 +20,9 @@ module.exports = {
     ],
     "@semantic-release/release-notes-generator",
     "@semantic-release/github",
-    "@semantic-release/git"
+    ["@semantic-release/git", {
+      "assets": ["package.json"],
+      "message": "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
+    }]
   ]
 }
